8219eed733400bfd8707846d8a59e613433293f1
Tien Tuyen <tuyennt@elcom.com.vn>
2022-02-22 16:08:00 +0700

    [Add] [Update gradle]

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # jetpack.ntspacet
 demo custom UI for Jetpack Compose
+
+* Build gradle note*
+    "gradlew build --warning-mode all" to find out deprecated features.
--- app/src/main/java/com/jetpack/ntspacet/ui/theme/Color.kt
+++ app/src/main/java/com/jetpack/ntspacet/ui/theme/Color.kt
@@ -1,8 +1,20 @@
 package com.jetpack.ntspacet.ui.theme
 
+import androidx.compose.material.Colors
 import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val SuspiciousColor = Color(0xFF0F0F0F)
+
+val Colors.specialCardTitle: Color
+    get() = SuspiciousColor
+
+val Colors.profileTitle: Color
+    get() = SuspiciousColor
+
+val Colors.settingFooter: Color
+    get() = SuspiciousColor
